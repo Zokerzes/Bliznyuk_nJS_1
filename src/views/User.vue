742acdde0d4ff9d1d7133a61009cd2fe92c3d992
Zokerzes <yozhik13@gmail.com>
2023-07-14 20:58:22 +0300

    ++

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,7 +1,19 @@
 <template>
   Страница пользователя
+  <Avatar />
 </template>
 
 <script>
+import Avatar from "@/components/Avatar.vue"
+export default {
+  name: "User",
+  components: {
+    Avatar,
+  },
+  data() {
+  },
+}
 
 </script>
+
+
