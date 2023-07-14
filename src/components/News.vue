c0e7e1185c2fde77ee113841a10adb3f92b1a50c
Zokerzes <yozhik13@gmail.com>
2023-07-14 21:17:29 +0300

    hw complite

---
+++ src/components/News.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="head">
+    Новости пользователя
+  </div>
+  <div class="text">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto quisquam doloribus laudantium, repudiandae
+    aperiam cum exercitationem ipsam ad eius. Aliquid, dolorem tempore neque nam iste laudantium iure omnis quis!
+  </div>
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  name: "News",
+  components: {
+  },
+  data() {
+  },
+}
+</script>
+
+<style>
+.head {
+  text-decoration-line: underline;
+}
+
+.text {
+  width: 20%;
+}
+</style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -2,16 +2,19 @@
   Страница пользователя
   <Avatar />
   <Info />
+  <News />
 </template>
 
 <script>
 import Avatar from "@/components/Avatar.vue"
 import Info from "@/components/UserInfo.vue"
+import News from "@/components/News.vue"
 export default {
   name: "User",
   components: {
     Avatar,
     Info,
+    News,
   },
   data() {
   },
